ac0285da82ea7aaa5cfd954f782d47b5565a1cc8
gaojunhao <197622534@qq.com>
2019-12-22 15:18:52 +0000

    number

--- data_type/stable/number.py
+++ data_type/stable/number.py
@@ -5,3 +5,24 @@ number:
     bool
     complex
 '''
+
+import random
+
+float_number = 3.98
+
+print(int(float_number))
+
+int_number_1 = 2
+
+int_number_2 = 4
+
+list = [int_number_1, int_number_2]
+
+print(max(list))
+
+print(pow(int_number_1, int_number_2))
+
+
+print(random.sample(range(20),10))
+
+print(random.choice(range(10)))
