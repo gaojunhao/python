23a3da07c04ba74276452f162568ca2fbd7ce732
Junhao Gao <junhao.gao@intel.com>
2019-12-25 14:50:39 +0800

    modify set.py

--- data_type/unstable/set.py
+++ data_type/unstable/set.py
@@ -10,3 +10,16 @@ print(set_1)
 print(set_2)
 
 print(set_1 - set_2)
+
+set_1 = set((34, 1+9j, 'a'))
+print(set_1)
+set_1.update([2, 4])
+set_1.add("gf")
+set_1.update({'d':89})
+print(set_1)
+
+set_1.remove('d')
+print(set_1)
+
+set_1.pop()
+print(set_1)
